9a47afc6e923a6a254c3d4ec6507792fe19913c1
tuanpham25 <tuan.pd.2233@aptechlearning.edu.vn>
2024-10-29 12:15:09 +0700

    fix image mapper

--- ProjectBackend/src/main/java/com/example/projectbackend/mapper/ImageMapper.java
+++ ProjectBackend/src/main/java/com/example/projectbackend/mapper/ImageMapper.java
@@ -1,4 +1,22 @@
 package com.example.projectbackend.mapper;
 
+import com.example.projectbackend.bean.request.ImageRequest;
+import com.example.projectbackend.bean.response.ImageResponse;
+import com.example.projectbackend.entity.Image;
+
 public class ImageMapper {
+    public static ImageResponse convertToResponse(Image image){
+        ImageResponse imageResponse = new ImageResponse();
+        imageResponse.setImageId(image.getImageId());
+        imageResponse.setImageFileName(image.getImageFileName());
+        imageResponse.setName(image.getName());
+        return imageResponse;
+    }
+
+    public static Image convertFromRequest(ImageRequest imageRequest){
+        Image image = new Image();
+        image.setName(imageRequest.getName());
+        image.setImageFileName(imageRequest.getImage());
+        return image;
+    }
 }
